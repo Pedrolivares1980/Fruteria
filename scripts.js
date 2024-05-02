c0d23311c9ca94c61600ffc42a8fa4ab14a15051
Pedrolivares1980 <pedr.olivares@yahoo.com>
2024-05-02 11:24:23 +0100

    ssnn

---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+// scroll up
+
+document.getElementById("button-up").addEventListener("click", scrollUp);
+
+function scrollUp() {
+    var currentScroll = document.documentElement.scrollTop;
+    if (currentScroll > 0) {
+        window.scrollTo(0, 0);
+    }
+}
